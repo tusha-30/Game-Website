8e626160e52c081fdbe78a4dbb7acc65e8df8fe7
Tusha <tusha@Tushas-MacBook-Air.local>
2024-09-04 17:33:43 +0530

    sidebar added

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SettingsPage = () => {
+  return (
+    <div>settings</div>
+  )
+}
+
+export default SettingsPage
--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,8 +1,8 @@
-"use client";  // Ensure this component is rendered on the client side
-
+"use client";
 import React, { ReactNode, useState } from 'react';
 import Topbar from './Topbar';
 import Sidebar from './Sidebar';
+import styles from './MainBody.module.css'; // Import your CSS module if using
 
 type MainBodyProps = {
   children: ReactNode;
@@ -17,9 +17,9 @@ const MainBody = ({ children }: MainBodyProps) => {
   };
 
   return (
-    <div className="flex-1 flex flex-col" style={{ marginLeft: `${sidebarWidth}px` }}>
+    <div className="flex-1 flex flex-col">
       <Topbar />
-      <main className="flex-1 overflow-y-auto">
+      <main className={`flex-1 overflow-y-auto ${styles.mainContent}`} style={{ marginLeft: `${sidebarWidth}px` }}>
         {children}
       </main>
       <Sidebar onWidthChange={handleSidebarWidthChange} />
